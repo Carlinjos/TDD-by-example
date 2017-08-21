fc49375830255cb3fb8de7d2d90fbef6bcbb3cd8
Carlinjos <carlososorio2@gmail.com>
2017-08-21 12:24:37 +0200

    First commit

---
+++ Dollar.java
@@ -0,0 +1,11 @@
+
+public class Dollar extends Money{
+	
+	Dollar(int amount, String currency) {
+		super(amount, currency);
+	}
+	
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+}
---
+++ Franc.java
@@ -0,0 +1,11 @@
+
+public class Franc extends Money {
+
+	Franc(int amount, String currency) {
+		super(amount, currency);
+	}
+	
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
+}
